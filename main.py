4ccbdf8a748ce5daaee783b78501a021b4594dc5
jansel <janselroa2424@gmail.com>
2021-10-02 11:48:42 -0400

    second commit

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
-@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.route("/articles/<n>")
+def articles(n:int):
+    try:
+        return render_template(f"article{n}.html")
+    except:
+        return render_template("404.html"),404
+        
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
 const darkBoton = document.querySelector(".dark-mode");
 const btnMenu = document.querySelector(".btn-menu");
 const menuUl = document.querySelector(".menu-ul")
-darkBoton.addEventListener("click", () => {
-    document.body.classList.toggle("white-mode")
+
+const cambiar_icon = () => {
     if (document.body.classList.contains("white-mode")) {
         darkBoton.children[0].textContent = "white mode";
         btnMenu.children[0].setAttribute("src", "/static/img/icon-menu-black.svg");
@@ -10,7 +10,22 @@ darkBoton.addEventListener("click", () => {
         btnMenu.children[0].setAttribute("src", "/static/img/icon-menu.svg");
         darkBoton.children[0].textContent = "dark mode"
     }
-})
+}
+cambiar_icon()
+
+if (localStorage.getItem('mode') == "white") {
+    document.body.classList.add("white-mode");
+};
+
+darkBoton.addEventListener("click", () => {
+
+    document.body.classList.toggle("white-mode");
+    localStorage.setItem("mode", "white");
+    cambiar_icon()
+    if (localStorage.getItem("mode") == "white") {
+        localStorage.setItem("mode", "dark");
+    }
+});
 btnMenu.addEventListener("click", () => {
     menuUl.classList.toggle("menu-shild")
 })
